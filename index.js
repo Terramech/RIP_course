535636926fdf5028dea88f042d1b7e689203924d
terramech <hidercreeper2.pro@gmail.com>
2026-04-11 00:08:48 +0300

    added transitions

--- index.js
+++ index.js
@@ -7,5 +7,23 @@ wikiButtons.forEach(function(button) {
         window.location.href = 'categories.html';
     });
 });
+let returnButton = document.querySelector('.returnBtn');
+returnButton.addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+// profile overlay code
+let profileButton = document.getElementById('profileTrigger');
+let profileOverlay = document.getElementById('profileOverlayBlock');
+profileOverlay.addEventListener('click', function(event) {
+    if (profileOverlay.classList.contains('active')) {
+        profileOverlay.classList.remove('active');
+    }
+});
+profileButton.addEventListener('click', function(event) {
+    profileOverlay.classList.add('active');
+    });
+
+
 
 
